da52ca2ad9027a2a14017358b8e4868f3c651f00
leeminjae <qlqlzbto@naver.com>
2023-10-27 18:55:24 +0900

    set without storyboard

--- DaumSports/ViewController.swift
+++ DaumSports/ViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
 
